1a7fe3282e2f5ff5e8f057b5f3360433a1917e7e
Patricia Walker <pwalker423@gmail.com>
2025-11-29 12:39:18 -0500

    Starting unit testing

--- test_ml.py
+++ test_ml.py
@@ -1,4 +1,8 @@
 import pytest
+import pandas as pd
+from ml.model import train_model
+from sklearn.datasets import  make_classification
+from sklearn.ensemble import RandomForestClassifier
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
@@ -6,9 +10,12 @@ def test_one():
     """
     # add description for the first test
     """
-    # Your code here
-    pass
 
+    X, y = make_classification(n_samples=100, n_features=200, random_state=42)
+    model = train_model(X,y)
+    print(model)
+
+    assert hasattr(model, "coef_"), "The model should have attributes after training"
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
